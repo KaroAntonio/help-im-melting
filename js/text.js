26cd531f92a13bcf260fe105ec30cea3fb8d7f41
Karo <karo@alyannas-iphone.ocad.ca>
2017-10-26 18:52:30 -0400

    distractions

--- js/text.js
+++ js/text.js
@@ -3,61 +3,63 @@
 function draw_text() {
 
 var idx = 0;
+var pay_button = $('<div>PAY $$$ TO STOP MELTING</div>');
+var ignore_button = $('<div class="highlight">IGNORE, U SLY FOX</div>');
 
 wrds = [
-	'oi vey',
+	'oi vey.',
 	'im',
 	'melting.',
-	'halp',
+	'halp.',
 	'im',
-	'very',
+	'really very',
 	'melting.',
-	'this is',
+	'it kinda hurts.',
+	'This is',
 	'a message',
 	'from a person',
 	'who is',
-	'currently melting.',
-	'this is',
-	'not sexy',
-	'please',
+	'currently melting:',
+	'im like a',
+	'kraft singles slice',
+	'on a windshield.',
+	'Aka not sexy.',
+	'Please',
 	'give me',
-	'strength',
-	'you could',
+	'ur coolness.',
+	'I want',
+	'to be frosty', 
+	'like youuuu.',
+	'Please dont',
 	'ignore me',
 	'...',
 	'id',
 	'hate that',
 	':(',
-	'PAY NOW',
-	'$$$',
-	'to stop',
-	'melting :)',	
-	'ONE TIME',
-	'OFFER!!!',
-	'50% OFF',
-	'u have',
-	'the power',
-	'<a href="http://www.worldlandtrust.org/">BUY NOW</a>',
+	'Ur like,',
+	'my hero',
 ]
 
 console.log('draw text')
 
 var overlay = $('<div>');
+var overlay_text = $('<div>');
 console.log('new');
 overlay.addClass('noselect');
 
-overlay.text('oy vey');
-
+overlay_text.text(wrds[idx]);
+overlay_text.appendTo(overlay);
 overlay.appendTo('body');
 
 overlay.css({
+	textAlign: 'center',
 	position:'fixed',
 	zIndex: 1000,
 	fontSize:110,
 	color: 'red',
 	width: window.innerWidth,
 	height: window.innerHeight,
-	paddingLeft: window.innerWidth/2-200,
+	//paddingLeft: window.innerWidth/2-200,
 	paddingTop: window.innerHeight/2-100,
 	userSelect: 'none',
 	'-webkit-user-select': 'none',
@@ -66,8 +68,64 @@ overlay.css({
 
 overlay.click(function() {
 	idx = (idx + 1) % wrds.length;
-	overlay.html(wrds[idx]);
+	overlay_text.html(wrds[idx]);
 	document.getSelection().removeAllRanges();
+
+	overlay.append(pay_button);
+	overlay.append(ignore_button);
 });
+	
+// BUTTONS
+
+ignore_button.hover(function() {
+	//$(this).css({'color':'yellow', 'cursor':'url(assets/sparkles.gif), auto'});
+	$(this).css({'color':'yellow', 'cursor':'pointer'});
+}, function() {
+	$(this).css({'color':'red'});
+});
+
+pay_button.hover(function() {
+	$(this).css({'color':'yellow', 'cursor':'pointer'});
+}, function() {
+	$(this).css({'color':'red'});
+});
+
+pay_button.click(function() {
+	window.location.replace("http://www.worldlandtrust.org/projects/buy-acre");
+});
+
+ignore_button.click(function() {
+	place_distraction();
+});
+
+overlay.append(pay_button);
+overlay.append(ignore_button);
+
+pay_button.css({
+	fontSize:40,
+	position:'fixed',
+	bottom: 100,
+	left: 100	
+});
+
+ignore_button.css({
+	fontSize:40,
+	position:'fixed',
+	bottom: 100,
+	right: 100	
+});
+
+function place_distraction() {
+	var dist_urls = ['sparkles','sparkles','sparkles','unicorn_hula','rainicorn','pugicorn']
+	var new_distract = $('<img>')
+	new_distract.attr('src','assets/'+dist_urls[Math.floor(Math.random() * dist_urls.length)]+'.gif');
+	new_distract.css({
+		position: 'fixed',
+		left: Math.random()*(window.innerWidth),
+		top: Math.random()*(window.innerHeight),
+	});
+	$('body').append(new_distract);
+}
+
 }
 
